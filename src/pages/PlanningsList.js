be23c69b1fd8e50e017058860e1aae8a9552bd6d
Ruben Andino <rubenanlo@gmail.com>
2022-08-07 13:48:01 +0200

    checkpoint

--- src/pages/PlanningsList.js
+++ src/pages/PlanningsList.js
@@ -1,7 +1,43 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 function PlanningsList() {
-  return <div>PlanningsList</div>;
+  const [weeklyPlans, setWeeklyPlans] = useState([]);
+  const storedToken = localStorage.getItem("authToken");
+  const background = ["../../week1.jpg", "../../week2.jpeg"];
+
+  useEffect(() => {
+    axios
+      .get(`${process.env.REACT_APP_API_URL}/weeklyplans`, {
+        headers: { Authorization: `Bearer ${storedToken}` },
+      })
+      .then((response) => setWeeklyPlans(response.data))
+      .catch((error) => console.log(error));
+  }, [storedToken]);
+
+  return (
+    <div>
+      {weeklyPlans?.map((weeklyPlan) => {
+        return (
+          <div
+            style={{
+              backgroundImage: `url(${
+                background[Math.floor(Math.random() * background.length)]
+              })`,
+              backgroundRepeat: "no-repeat",
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+            }}
+            key={weeklyPlan._id}
+          >
+            <h3>{weeklyPlan.startDate}</h3>
+            <h1>{weeklyPlan.mealType}</h1>
+            <h1>{weeklyPlan.weeklyRecipes}</h1>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default PlanningsList;
