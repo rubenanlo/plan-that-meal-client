436670dd2c53671701b075fd3196e201263d7747
Ruben Andino <rubenanlo@gmail.com>
2022-08-16 16:34:56 +0200

    checkpoint

--- src/components/PlanningsMain.js
+++ src/components/PlanningsMain.js
@@ -1,6 +1,7 @@
 import axios from "axios";
-import React, { useEffect, useState } from "react";
-import { Route, Routes } from "react-router-dom";
+import React, { useCallback, useContext, useEffect, useState } from "react";
+import { Link, Route, Routes } from "react-router-dom";
+import { AuthContext } from "../context/auth.context";
 import PlanningsDetails from "../pages/PlanningsDetails";
 import PlanningsList from "../pages/PlanningsList";
 import "./PlanningsMain.css";
@@ -8,45 +9,66 @@ import "./PlanningsMain.css";
 function PlanningsMain() {
   const [weeklyPlans, setWeeklyPlans] = useState([]);
   const storedToken = localStorage.getItem("authToken");
+  const { isLoading } = useContext(AuthContext);
 
-  useEffect(() => {
-    getAllWeeklyPlans();
-  }, []);
-
-  const getAllWeeklyPlans = () => {
+  const getAllWeeklyPlans = useCallback(() => {
     axios
       .get(`${process.env.REACT_APP_API_URL}/weeklyplans`, {
         headers: { Authorization: `Bearer ${storedToken}` },
       })
       .then((response) => setWeeklyPlans(response.data))
       .catch((error) => console.log(error));
-  };
+  }, [storedToken]);
+
+  useEffect(() => {
+    getAllWeeklyPlans();
+  }, [getAllWeeklyPlans]);
 
   return (
     <div>
-      <div>
-        <div className="container">
-          <div>
-            <PlanningsList
-              weeklyPlans={weeklyPlans}
-              refreshWeeklyPlans={getAllWeeklyPlans}
-            />
-          </div>
-        </div>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
         <div>
-          <Routes>
-            <Route
-              path="/:weeklyPlanId"
-              element={
-                <PlanningsDetails
-                  details={weeklyPlans}
+          {weeklyPlans.length === 0 && (
+            <div>
+              <p>
+                Not a weekly plan yet? Not to worry, we've got you covered, you
+                can create your first one{" "}
+                <Link to="/weeklyplans/create">here</Link>
+              </p>
+              <img
+                style={{ width: "20vw" }}
+                src="../../empty-recipe.jpeg"
+                alt=""
+              />
+            </div>
+          )}
+          <div>
+            <div className="container">
+              <div>
+                <PlanningsList
+                  weeklyPlans={weeklyPlans}
                   refreshWeeklyPlans={getAllWeeklyPlans}
                 />
-              }
-            ></Route>
-          </Routes>
+              </div>
+            </div>
+            <div>
+              <Routes>
+                <Route
+                  path="/:weeklyPlanId"
+                  element={
+                    <PlanningsDetails
+                      details={weeklyPlans}
+                      refreshWeeklyPlans={getAllWeeklyPlans}
+                    />
+                  }
+                ></Route>
+              </Routes>
+            </div>
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 }
--- src/pages/PlanningsList.js
+++ src/pages/PlanningsList.js
@@ -1,52 +1,30 @@
 import moment from "moment";
-import { useContext, useEffect } from "react";
-import { Link } from "react-router-dom";
-import { AuthContext } from "../context/auth.context";
+import { useEffect } from "react";
 
 function PlanningsList(props) {
-  const { isLoading } = useContext(AuthContext);
-
   useEffect(() => {
     props.refreshWeeklyPlans();
   }, []);
 
   return (
     <>
-      {isLoading ? (
-        <p>Loading...</p>
-      ) : (
-        <div>
-          {props.weeklyPlans?.length === 0 && (
-            <div>
-              <p>
-                Not a weekly plan yet? Not to worry, we've got you covered, you
-                can create your first one{" "}
-                <Link to="/weeklyplans/create">here</Link>
-              </p>
-              <img
-                style={{ width: "20vw" }}
-                src="../../empty-recipe.jpeg"
-                alt=""
-              />
-            </div>
-          )}
-          {props.weeklyPlans?.map((weeklyPlan) => {
-            return (
-              <a
-                className="link"
-                href={`/weeklyplans/${weeklyPlan._id}`}
-                key={weeklyPlan._id}
-              >
-                <div>
-                  <h3 className="weekly-plan">
-                    {moment(weeklyPlan.startDate).format("dddd, DD MMMM YYYY")}
-                  </h3>
-                </div>
-              </a>
-            );
-          })}
-        </div>
-      )}
+      <div>
+        {props.weeklyPlans?.map((weeklyPlan) => {
+          return (
+            <a
+              className="link"
+              href={`/weeklyplans/${weeklyPlan._id}`}
+              key={weeklyPlan._id}
+            >
+              <div>
+                <h3 className="weekly-plan">
+                  {moment(weeklyPlan.startDate).format("dddd, DD MMMM YYYY")}
+                </h3>
+              </div>
+            </a>
+          );
+        })}
+      </div>
     </>
   );
 }
