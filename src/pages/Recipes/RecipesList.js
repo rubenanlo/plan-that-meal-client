36888b37b8bb2a333a29ee0cb8605b2594dec82b
Ruben Andino <rubenanlo@gmail.com>
2022-08-20 17:43:02 +0200

    reshuffled files

---
+++ src/pages/Recipes/RecipesList.js
@@ -0,0 +1,104 @@
+import axios from "axios";
+import { useContext, useEffect, useState } from "react";
+import { Link, useSearchParams } from "react-router-dom";
+import { AuthContext } from "../../context/auth.context";
+import "./RecipesList.css";
+
+function RecipesList() {
+  const { isLoading } = useContext(AuthContext);
+  const storedToken = localStorage.getItem("authToken");
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`${process.env.REACT_APP_API_URL}/recipes`, {
+        headers: { Authorization: `Bearer ${storedToken}` },
+      })
+      .then((response) => setRecipes(response.data))
+      .catch((error) => console.log(error));
+  }, [storedToken]);
+
+  const [searchParams, setSearchParams] = useSearchParams();
+  const searchTerm = searchParams.get("protein") || "";
+  const handleSearch = (e) => {
+    const protein = e.target.value;
+    return protein ? setSearchParams({ protein }) : setSearchParams({});
+  };
+
+  const renderRecipes = () => {
+    let result;
+    searchTerm !== "All"
+      ? (result = recipes
+          .filter((recipe) => recipe.protein.includes(searchTerm))
+          .map((recipe) => {
+            return (
+              <div className="recipe" key={recipe._id}>
+                <Link className="link-no-format" to={`/recipes/${recipe._id}`}>
+                  <h3>{recipe.title}</h3>
+                  <img src={recipe.img} alt="recipe" />
+                </Link>
+                <div className="divider"></div>
+              </div>
+            );
+          }))
+      : (result = recipes.map((recipe) => {
+          return (
+            <div className="recipe" key={recipe._id}>
+              <Link className="link-no-format" to={`/recipes/${recipe._id}`}>
+                <h3>{recipe.title}</h3>
+                <img src={recipe.img} alt="recipe" />
+              </Link>
+              <div className="divider"></div>
+            </div>
+          );
+        }));
+
+    return result;
+  };
+
+  return (
+    <div>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <div>
+          <div className="search">
+            <form onChange={handleSearch}>
+              <select
+                type="text"
+                name="protein"
+                value={searchTerm}
+                placeholder="search by protein"
+                onChange={(e) => {
+                  setSearchParams({ protein: e.target.value });
+                }}
+              >
+                <option value={""}>Search by type of protein</option>
+                <option>Meat</option>
+                <option>Fish</option>
+                <option>Eggs</option>
+                <option>Legumes</option>
+                <option>Seeds and nuts</option>
+              </select>
+            </form>
+          </div>
+          <div className="recipes">
+            {recipes.length === 0 ? (
+              <div style={{ marginLeft: "28vw" }}>
+                <p>
+                  Be the first to create a new recipe{" "}
+                  <Link to="/recipes/create">here</Link>
+                </p>
+                <img src="../../empty-recipe.jpeg" alt="" />
+              </div>
+            ) : (
+              renderRecipes()
+            )}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default RecipesList;
