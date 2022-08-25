d76b8b4f76a109d734aa76625f29099e829458b7
Ruben Andino <rubenanlo@gmail.com>
2022-08-25 23:42:47 +0200

    fixed some typos

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -9,7 +9,7 @@ function Navbar() {
   return (
     <>
       {isLoading ? (
-        <p></p>
+        <p>loading...</p>
       ) : (
         <nav className="navbar">
           <div>
--- src/components/Navbar/NavbarItems.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function NavbarItems() {
-  return <div>NavbarItems</div>;
-}
-
-export default NavbarItems;
